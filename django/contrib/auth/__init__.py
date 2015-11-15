631428776fafe082a76062a3ec3152925d042b00
hdknr <gmail@hdknr.com>
2015-11-15 10:37:11 +0900

    セッションキーよりユーザーを特定

--- django/contrib/auth/__init__.py
+++ django/contrib/auth/__init__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import inspect
 import re
 
@@ -56,6 +57,7 @@ def _clean_credentials(credentials):
 def _get_user_session_key(request):
     # This value in the session is always serialized to a string, so we need
     # to convert it back to Python whenever we access it.
+    # これでuser_id がわかります
     return get_user_model()._meta.pk.to_python(request.session[SESSION_KEY])
 
 
